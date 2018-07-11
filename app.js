430b8b02ee503ee0e8c06c5460bfc42bff42f19c
ellierui <ellie.ruiyilinbca@gmail.com>
2018-07-11 14:49:34 -0700

    controllers made

---
+++ app.js
@@ -0,0 +1,29 @@
+
+// Budget Controller
+var budgetController = (function(){
+
+  
+})();
+
+
+
+// UI Controller
+var UIController = (function(){
+
+
+})();
+
+
+// Global App Controller
+var controller = (function(budgetCtrl, UICtrl){
+
+    document.querySelector('.add__btn').addEventListener('click', function(){
+        
+        // 1. get the field input data
+        // 2. add the item to the budget controller
+        // 3. add the item to the UI
+        // 4. calculate the budget
+        // 5. Display the budget on UI
+    })
+
+})(budgetController,UIController);
